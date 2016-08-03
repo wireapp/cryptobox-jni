e77c44386fa599b487168ac05b41994448280fa7
Sven Jost <sven@wire.com>
2016-08-03 15:08:02 +0200

    Only use System.loadLibrary if slim-jar and only use libraries from JAR if fat-jar

--- src/java/com/wire/cryptobox/NativeLibraryLoader.java
+++ src/java/com/wire/cryptobox/NativeLibraryLoader.java
@@ -9,38 +9,42 @@ public class NativeLibraryLoader {
     private static File tempDir = null;
 
     public static void loadLibrary(String libname) {
-        try {
+        if (NativeLibraryLoader.class.getResource("/fatjar") != null) {
+            loadFormJar(libname);
+        } else {
             System.loadLibrary(libname);
-        } catch (UnsatisfiedLinkError ue) {
-            try {
-                String filename = System.mapLibraryName(libname);
+        }
+    }
 
-                if (tempDir == null) {
-                    tempDir = createTempDirectory("native");
-                }
-                File tempFile = new File(tempDir.getAbsolutePath(), filename);
+    private static void loadFormJar(String libname) {
+        String filename = System.mapLibraryName(libname);
 
-                InputStream istream = CryptoBox.class.getResourceAsStream("/" + filename);
-                if (istream == null) {
-                    throw new FileNotFoundException("Could not find native library " + libname + " in jar.");
-                }
-
-                byte[] buffer = new byte[1024];
-                int bytes;
-                OutputStream ostream = new FileOutputStream(tempFile);
-                try {
-                    while ((bytes = istream.read(buffer)) != -1) {
-                        ostream.write(buffer, 0, bytes);
-                    }
-                } finally {
-                    ostream.close();
-                    istream.close();
-                }
-
-                System.load(tempFile.getAbsolutePath());
-            } catch (IOException ie) {
-                throw new RuntimeException(ie);
+        try {
+            if (tempDir == null) {
+                tempDir = createTempDirectory("native");
             }
+            File tempFile = new File(tempDir.getAbsolutePath(), filename);
+
+            InputStream istream = CryptoBox.class.getResourceAsStream("/" + filename);
+            if (istream == null) {
+                throw new FileNotFoundException("Could not find native library " + filename + " in jar.");
+            }
+
+            byte[] buffer = new byte[1024];
+            int bytes;
+            OutputStream ostream = new FileOutputStream(tempFile);
+            try {
+                while ((bytes = istream.read(buffer)) != -1) {
+                    ostream.write(buffer, 0, bytes);
+                }
+            } finally {
+                ostream.close();
+                istream.close();
+            }
+
+            System.load(tempFile.getAbsolutePath());
+        } catch (IOException ie) {
+            throw new RuntimeException(ie);
         }
     }
 
