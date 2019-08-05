339e856f6751e572126b31eeeef41ff6f37aa18d
Marco Conti <marcoconti83@gmail.com>
2019-08-05 15:40:02 +0200

    Use proper cryptobox version

--- mk/cryptobox-src.mk
+++ mk/cryptobox-src.mk
@@ -1,4 +1,4 @@
-CRYPTOBOX_VERSION := v1.1.0
+CRYPTOBOX_VERSION := v1.1.2
 CRYPTOBOX_NAME    := cryptobox-$(CRYPTOBOX_VERSION)
 CRYPTOBOX_GIT_URL := https://github.com/wireapp/cryptobox-c.git
 
@@ -7,4 +7,4 @@ build/src/$(CRYPTOBOX_NAME):
 	cd build/src && \
 	git clone $(CRYPTOBOX_GIT_URL) $(CRYPTOBOX_NAME) && \
 	cd $(CRYPTOBOX_NAME) && \
-	git checkout test_bumping_libs
+	git checkout $(CRYPTOBOX_VERSION)
