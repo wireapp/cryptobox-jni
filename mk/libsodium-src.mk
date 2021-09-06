262b08953322f19791f9d20dc663b6f82308f97f
jschaul <jschaul@users.noreply.github.com>
2021-09-06 21:45:14 +0200

    Use https url.
    Since #18 is apparently much more controversial than I anticipated, this is a hopefully uncontroversial incremental improvement over the status quo.
    
    Fixes #16

--- mk/libsodium-src.mk
+++ mk/libsodium-src.mk
@@ -1,6 +1,6 @@
 LIBSODIUM_VERSION := 1.0.18
 LIBSODIUM_NAME    := libsodium-$(LIBSODIUM_VERSION)
-LIBSODIUM_URL     := http://download.libsodium.org/libsodium/releases/$(LIBSODIUM_NAME).tar.gz
+LIBSODIUM_URL     := https://download.libsodium.org/libsodium/releases/$(LIBSODIUM_NAME).tar.gz
 
 build/src/$(LIBSODIUM_NAME):
 	mkdir -p build/src
